75937d3f264cced184e2e57c7254b2f7058be31e
jashank01 <jkaur769@my.centennialcollege.ca>
2022-07-19 14:18:34 -0400

    first commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = 
 {
-    // "URI": "mongodb://localhost:27017",
+    // "URI": "mongodb://localhost:27017",dsasd
     "Secret": 'SomeSecret',
     "URI": "mongodb+srv://jashanpreetkaur:4PCHL8w52SYRB4i@cluster0.znnv5tw.mongodb.net/test",
 }   
